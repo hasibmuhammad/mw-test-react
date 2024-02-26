ccf1c33c922f8cc3a1eb014a058cceb4af79565e
hasibmuhammad <hasibmu99@gmail.com>
2024-02-26 12:22:23 +0600

    Problem2 - Buttons added

--- src/components/Problem2.jsx
+++ src/components/Problem2.jsx
@@ -1,6 +1,21 @@
 const Problem2 = () => {
+
+    const handleAllContacts = (e) => {
+        // pass
+    }
+
+    const handleUsContacts = (e) => {
+        // pass
+    }
+
   return (
-    <div>Problem2</div>
+    <div className="mt-20 flex flex-col justify-center items-center">
+        <h1 className="mb-10 text-3xl font-bold uppercase">Problem 2</h1>
+        <div className="flex gap-5">
+          <button onClick={handleAllContacts} className="btn btn-primary btn-outline">All Contacts</button>
+          <button onClick={handleUsContacts} className="btn btn-success btn-outline">US Contacts</button>
+        </div>
+    </div>
   )
 }
 
