bb1d71579dcbd3c65bdbe5b5ab0a5afe6e32a2d7
hasibmuhammad <hasibmu99@gmail.com>
2024-02-26 13:54:17 +0600

    Modifed the title and the buttons are outlined

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom"
 const Navbar = () => {
   return (
     <div className={`flex justify-center items-center gap-10`}>
-        <NavLink className={({isActive}) => `btn hover:btn-primary active:btn-primary focus:btn-primary font-bold ${isActive ? "bg-primary text-white":""}`} to={"/problem1"}>Problem1</NavLink>
-        <NavLink className={({isActive}) => `btn hover:btn-primary active:btn-primary focus:btn-primary font-bold ${isActive ? "bg-primary text-white":""}`} to={"/problem2"}>Problem2</NavLink>
+        <NavLink className={({isActive}) => `btn btn-outline hover:btn-primary active:btn-primary focus:btn-primary font-bold ${isActive ? "bg-primary text-white":""}`} to={"/problem1"}>Problem1</NavLink>
+        <NavLink className={({isActive}) => `btn btn-outline hover:btn-primary active:btn-primary focus:btn-primary font-bold ${isActive ? "bg-primary text-white":""}`} to={"/problem2"}>Problem2</NavLink>
     </div>
   )
 }
