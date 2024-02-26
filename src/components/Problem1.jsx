ac9f4f2e73076c7444751c58b74cfef5fc375478
hasibmuhammad <hasibmu99@gmail.com>
2024-02-26 11:57:12 +0600

    form added for problem 1

--- src/components/Problem1.jsx
+++ src/components/Problem1.jsx
@@ -1,6 +1,18 @@
 const Problem1 = () => {
   return (
-    <div>Problem1</div>
+    <div className="mt-20 flex flex-col justify-center items-center">
+        <h1 className="mb-10 text-3xl font-bold uppercase">Problem 1</h1>
+
+        <form>
+            <input required className="border border-orange-400 outline-none p-2 rounded-lg mr-2" type="text" name="title" id="" placeholder="Name" />
+            <select required className="border border-orange-400 p-2 rounded-lg mr-2 outline-none">
+                <option value="">Select</option>
+                <option value="active">Active</option>
+                <option value="completed">Completed</option>
+            </select>
+            <button className="bg-orange-400 px-4 py-2 rounded-lg font-bold text-white">Submit</button>
+        </form>
+    </div>
   )
 }
 
